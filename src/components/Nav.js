f4e2bc4dc385946de6843aedc6b4aec88302f058
Yi Lin <yilin@vl965-172-31-80-19.wireless.umass.edu>
2023-03-09 23:37:55 -0500

    test netlify change color to original

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     <div>
       <nav
         className="navbar navbar-expand-lg"
-        style={{ backgroundColor: "#F4CCD8" }}
+        style={{ backgroundColor: "#212529" }}
       >
         <div className="navbar-brand">
           <Link to="/" style={{ color: "#FFFFFF", textDecoration: "none" }}>
